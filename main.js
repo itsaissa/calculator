26f974c430c83d4b4971583ff64df46ea1082638
ItsaIssa <44656132+itsaissa@users.noreply.github.com>
2022-11-13 11:01:49 -0800

    Create calculator operation function

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ function add(a,b){return a+b;}
 function subtract(a,b){return Math.abs(a-b)} 
 function multiply(a,b){return a*b}
 function divide(a,b){return (a/b).toFixed(4)}
+
+function operate(operator, a,b){
+    let val = undefined;
+    switch(operator){
+        case "+":
+            val = add(a,b);
+            break;
+        case "-":
+            val = subtract(a,b);
+            break;
+        case "*":
+            val = multiply(a,b);
+            break;
+        case "/":
+            val = divide(a,b);
+            break;
+    }
+    return val;
+}
